388edaf3ba4bf0a60d6481bddb67ec6807f43c05
Mat26045 <mat26045@email.vccs.edu>
2024-09-26 21:54:07 -0400

    Add files via upload

---
+++ Ch2.Ex07-Mtilley.cpp
@@ -0,0 +1,19 @@
+// Ch2.Ex18-Mtilley.cpp 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double MIL_PER_YEAR = 1.5;
+
+    cout << endl;
+    cout << "The ocean's level will increase " << MIL_PER_YEAR * 5;
+    cout << " millimeters after 5 years. " << endl;
+    cout << "The ocean's level will rise " << MIL_PER_YEAR * 7;
+    cout << " millimeters after 7 years. " << endl;
+    cout << "The ocean's level will rise " << MIL_PER_YEAR * 10;
+    cout << " millimeter after 10 years." << endl;
+    cout << endl;
+}
+
